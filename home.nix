696b244e50f1a3b92e2978ca42824ff9b450a1c4
Michael Kohl <me@citizen428.net>
2021-12-29 14:22:30 +0000

    Basic home.nix

---
+++ home.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "nixos";
+  home.homeDirectory = "/home/nixos";
+
+  home.packages = [
+    pkgs.asdf-vm
+    pkgs.exa
+    pkgs.fishPlugins.foreign-env
+    pkgs.fnm
+    pkgs.htop
+    pkgs.starship
+  ];
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
+    }))
+  ];
+
+  programs.neovim = {
+    enable = true;
+    package = pkgs.neovim-nightly;
+  };
+}
